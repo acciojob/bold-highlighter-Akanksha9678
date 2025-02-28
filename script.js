b7e45cb4bc07c193c53af1397936ea10145a2d37
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-02-28 12:50:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,16 +2,14 @@ function highlight() {
     //Write your code here
 	let strong=document.querySelectorAll("strong")
 	for(let t of strong){
-		t.style.color=rgb(0, 128, 0);
+		t.style.color="rgb(0, 128, 0)";
 	}
-
 }
-
-
 function return_normal() {
     //Write your code here
+	let strong=document.querySelectorAll("strong")
 	for(let t of strong){
-		t.style.color=rgb(0, 0, 0);
+		t.style.color="rgb(0, 0, 0)";
 	}
     
 }
